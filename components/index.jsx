19c3b2be9e14dd744352e09f9377c209ec9671de
Matthew Daly <matthewbdaly@gmail.com>
2015-09-26 15:36:45 +0100

    Some progress

--- components/index.jsx
+++ components/index.jsx
@@ -1,7 +1,9 @@
 var React = require('react');
 var Tweets = require('./tweets.jsx');
 
+var initialState = [];
+
 React.render(
-  <Tweets />,
+  <Tweets data={initialState} />,
   document.getElementById('view')
 );
--- components/tweets.jsx
+++ components/tweets.jsx
@@ -1,12 +1,26 @@
 var React = require('react');
+var io = require('socket.io-client');
 var TweetList = require('./tweetlist.jsx');
 
 var Tweets = React.createClass({
+  componentDidMount: function () {
+    // Set up the connection
+    var socket = io.connect(window.location.href);
+
+    // Handle incoming messages
+    socket.on('message', function (data) {
+        // Insert the message
+        console.log(data);
+    });
+  },
+  getInitialState: function () {
+    return {data: this.props.data};
+  },
   render: function () {
     return (
       <div>
         <h1>Tweets</h1>
-        <TweetList />
+        <TweetList data={this.props.data} />
       </div>
     )
   }
