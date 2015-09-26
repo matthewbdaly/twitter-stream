0a0f5c318e3c2b985f3891b9435e56c00a1f6570
Matthew Daly <matthewbdaly@gmail.com>
2015-09-26 17:28:33 +0100

    Amended index component

--- components/index.jsx
+++ components/index.jsx
@@ -1,7 +1,8 @@
 var React = require('react');
 var Tweets = require('./tweets.jsx');
 
-var initialState = [];
+var initialState = JSON.parse(document.getElementById('initial-state').innerHTML);
+debugger;
 
 React.render(
   <Tweets data={initialState} />,
