9c6497f0a45cc523ee384883dc727351f2ddb4a7
Matthew Daly <matthewbdaly@gmail.com>
2015-09-26 14:17:30 +0100

    Can now stream tweets

---
+++ index.js
@@ -0,0 +1,18 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+ 
+  stream.on('error', function(error) {
+    throw error;
+  });
+});
